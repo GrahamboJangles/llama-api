634d8e5cd2b4f76dc35c7a44b932fae734190659
Graham <36944031+GrahamboJangles@users.noreply.github.com>
2023-04-04 18:30:11 -0400

    Create api.py

---
+++ api.py
@@ -0,0 +1,12 @@
+from api import llama
+
+prompt = input("Instruction: ")
+preprompt = f"""### Instruction: {prompt}\n### Response:"""
+response = llama(preprompt)
+try:
+    response = response.replace(f"### Instruction: {prompt}", "")
+    response = response.replace("### Response: ", "")
+    response = response.replace("### Response:", "")
+except UnicodeDecodeError as e:
+    pass
+                    
